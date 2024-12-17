d22f3501f3f4a78e9f901cc6788425f8c75f809c
Patrick reis <patrickwreis@gmail.com>
2024-12-17 22:10:40 +0000

    centraliza a mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <div>Se você me ama dê uma risadinha!</div>
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', color: '#333', fontSize: '24px' }}>
+      Se você me ama, dê uma risadinha! 😊
+    </div>
+  );
 }
 
 export default Home;
