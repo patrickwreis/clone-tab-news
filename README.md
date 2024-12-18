877753420e364f18e26e2e4c549b64cba77cfc46
Patrick reis <patrickwreis@gmail.com>
2024-12-18 13:09:57 +0000

    adicionar o 'lint:fix' e o 'lint:check'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tab-news
+
 Implementação do curso.dev do site tab-news para estudos de ponta a ponta do projeto
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', color: '#333', fontSize: '24px' }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        color: "#333",
+        fontSize: "24px",
+      }}
+    >
       Se você me ama, dê uma risadinha! 😊
     </div>
   );
