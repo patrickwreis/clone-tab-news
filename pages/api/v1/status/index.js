e5584fdc6db65bb3f2ef8677192b3942c4b1d96e
Patrick reis <patrickwreis@gmail.com>
2024-12-24 13:48:39 +0000

    finaliza o endpoint `api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Valor" });
+  const updateAt = new Date().toISOString();
+  const resultPostgresVersion = await database.query("SHOW server_version;");
+  const resultMaxConnection = await database.query("SHOW max_connections;");
+
+  const databaseName = process.env.POSTGRES_DB;
+  const resultOpenedConnection = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+
+  response.status(200).json({
+    update_at: updateAt,
+    database: {
+      dependencies: {
+        version: resultPostgresVersion.rows[0].server_version,
+        max_connections: parseInt(resultMaxConnection.rows[0].max_connections),
+        opened_connections: resultOpenedConnection.rows[0].count,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,18 @@
 test("GET to /api/v1/status deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  console.log(response.status);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(parsedUpdateAt).toBe(responseBody.update_at);
+
+  expect(responseBody.database.dependencies.version).toBe("16.6");
+  expect(parseInt(responseBody.database.dependencies.max_connections)).toBe(
+    100,
+  );
+  expect(parseInt(responseBody.database.dependencies.opened_connections)).toBe(
+    1,
+  );
 });
