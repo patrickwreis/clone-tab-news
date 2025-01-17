31fe581d44b3aa079937f7877926ba1d969cdbf9
Patrick reis <patrickwreis@gmail.com>
2025-01-17 15:07:02 +0000

    add `api/v1/migrations`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,8 +1,18 @@
+import database from "infra/database.js";
+
+beforeAll(clearDatabase);
+
+async function clearDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
 test("GET to /api/v1/migrations deve retornar 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
+  // console.log(responseBody);
 
   expect(Array.isArray(responseBody)).toBe(true);
+  expect(responseBody.length).toBeGreaterThan(0);
 });
